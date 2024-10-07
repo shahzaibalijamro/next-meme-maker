3ba00a3e8e118b8aa7c626ed75d8f0d320dfb288
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-10-08 02:59:46 +0500

    commands added

--- app/singlememe/[id]/page.tsx
+++ app/singlememe/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 interface SingleMeme {
     searchParams: {
@@ -14,6 +14,7 @@ interface SingleMeme {
     }
 }
 const SingleMeme = (props: SingleMeme) => {
+    const [returnedMeme,SetReturnedMeme] = useState(null);
     console.log(props.searchParams.box_count);
     const text1 = useRef<HTMLInputElement | null>(null);
     const text2 = useRef<HTMLInputElement | null>(null);
@@ -21,8 +22,8 @@ const SingleMeme = (props: SingleMeme) => {
     const text4 = useRef<HTMLInputElement | null>(null);
     const text5 = useRef<HTMLInputElement | null>(null);
     const text6 = useRef<HTMLInputElement | null>(null);
-    const refArr = [text1,text2,text3,text4,text5,text6];
-    const generateMeme = async() => {
+    const refArr = [text1, text2, text3, text4, text5, text6];
+    const generateMeme = async () => {
         let apiToHit = `https://api.imgflip.com/caption_image?template_id=${props.searchParams.id}&username=shahzaibalijamro&password=Iwasbornon29072006`
         for (let i = 0; i < props.searchParams.box_count; i++) {
             apiToHit += `&text${i}=${refArr[i].current!.value}`;
@@ -31,27 +32,38 @@ const SingleMeme = (props: SingleMeme) => {
         const singleMemeReq = await fetch(apiToHit, {
             method: 'POST'
         })
-        console.log(apiToHit);
-        const generatedMeme = await singleMemeReq.json();
+        const generatedMeme = await singleMemeReq.json()
+        SetReturnedMeme(generatedMeme.data.url)
         console.log(generatedMeme);
     }
     return (
         <div className='my-container'>
             <h1 className='font-semibold text-3xl mt-8 mb-12 text-black text-center'><span className='me-2'>&#128516;</span>{props.searchParams.name}
             </h1>
-            <div className='flex justify-center w-full items-center'>
-                <Image src={props.searchParams.img} width={400} height={400} alt={props.searchParams.name} />
-            </div>
-            <div className='text-center my-6'>
-                {Array.from({ length: props.searchParams.box_count }).map((_, index) => {
-                    return <div key={index}>
-                        <div className="input-group">
-                            <label className="label">Text {index+1}</label>
-                            <input autoComplete="off" name="Email" id="Email" className="input" placeholder={`Enter text ${index+1}`} type="email" ref={refArr[index]}/>
+            <div className='flex justify-around flex-wrap gap-10 items-center'>
+                <div className='flex flex-col justify-center w-full max-w-[400px] items-center'>
+                <h1 className='text-2xl font-semibold mb-4'>Meme Template</h1>
+                    <Image src={props.searchParams.img} width={320} height={320} alt={props.searchParams.name} />
+                </div>
+                <div className='text-center max-w-[400px] w-full my-6'>
+                    {Array.from({ length: props.searchParams.box_count }).map((_, index) => {
+                        return <div key={index}>
+                            <div className="input-group">
+                                <label className="label">Text {index + 1}</label>
+                                <input autoComplete="off" name="Email" id="Email" className="input" placeholder={`Enter text ${index + 1}`} type="email" ref={refArr[index]} />
                                 <div></div></div>
-                    </div>
-                })}
-                <button className='btn btn-warning mt-6' onClick={generateMeme}>Generate</button>
+                        </div>
+                    })}
+                    <button className='btn btn-warning mt-6' onClick={generateMeme}>Generate</button>
+                </div>
+                <div className='flex flex-col justify-center mb-10 w-full max-w-[400px] items-center'>
+                    {returnedMeme ? 
+                    <>
+                    <h1 className='text-2xl font-semibold mt-12 mb-6'>Result</h1>
+                    <Image src={returnedMeme} width={300} height={300} alt={props.searchParams.name} />
+                    </>
+                    : null}
+                </div>
             </div>
         </div>
     )
